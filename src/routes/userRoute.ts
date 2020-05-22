62ced785d90a83f4f527b6470ff270650e767355
dabao <1213365465@qq.com>
2020-05-22 17:30:17 +0800

    sjhdbc

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,17 +1,25 @@
 import Express from 'express';
 import session from 'express-session';
 import { UserService } from '../services/userServices';
+import { ResponseHelper } from './responseHelper';
 
 const router = Express.Router();
 // 登陆接口
 // 返回的参数 {msg:'xxx',err:1,user:{username,password}}
 router.post('/', async (req, res, next) => {
-  // const { username, password } = req.body;
-  // const obj: any = { username, password };
-  // const data = await UserService.find(obj);
-  // console.log(data);
-  console.log(req.body);
-  res.send('hello');
+  const { username, password } = req.body;
+  const obj: any = { username, password };
+  const data = await UserService.find(obj);
+
+  if (data.count > 0) {
+    const result = 1;
+    ResponseHelper.sendData(result, res);
+    console.log(result, res);
+  } else {
+    const result = 0;
+    ResponseHelper.sendData(result, res);
+    console.log(result, res);
+  }
 });
 
 export default router;
